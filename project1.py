9606b15f54373a9195d4f1d3ff7f480e8ee1bc25
Hamza7655code <mughis.rehman@yahoo.com>
2025-06-01 19:01:59 +0100

    Update project1.py

--- project1.py
+++ project1.py
@@ -1,8 +1,10 @@
-decimal = int(input("Enter in a decimal number: "))
+decimal = int(input("Enter a decimal number: "))
 def decimal_to_binary(decimal):
+    if decimal == 0:
+        return '0'
     binary = ''
     while decimal > 0:
-        remainder = decimal % 2
-        decimal = decimal // 2
-        binary = str(remainder) + binary
+        binary = str(decimal % 2) + binary
+        decimal //= 2
     return binary
+print(decimal_to_binary(decimal))
